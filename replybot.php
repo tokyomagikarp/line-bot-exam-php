02f8c277fbcd0708df81e44a5a3805663708e1b9
tokyomagikarp <97447023+tokyomagikarp@users.noreply.github.com>
2022-01-21 14:23:40 +0700

    create replybot

--- replybot.php
+++ replybot.php
@@ -21,7 +21,7 @@ foreach ($events['events'] as $event) {
 // Reply only when message sent is in 'text' format
 if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
-$text = $event['source']['userId'];
+$text = $event['source']['message'];
 // Get replyToken
 $replyToken = $event['replyToken'];
 // Build message to reply back
