36362740ac3bd7f024b8f9751159154f4c3baa6a
tokyomagikarp <97447023+tokyomagikarp@users.noreply.github.com>
2022-01-21 13:31:02 +0700

    edit webhooks

--- botpush.php
+++ botpush.php
@@ -9,17 +9,18 @@ $access_token = '4Bp3ON6lBN7sUZbvQ/12pOlUnWMjSg9on7Avj6vmtll013Km4kPtGZvJhblSiV8
 $channelSecret = 'a442994d2bf1499fd9b2b771d362e7dd';
 
 $pushID = 'U31164826a61dc970957e3edc089f87c0';
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-foreach ($events['events'] as $event) {
-$texttest = $event['source']['userId'];
-$response = $bot->pushMessage($pushID, $texttest);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-}
+
+
+
+
+
+
+
--- webhooks.php
+++ webhooks.php
@@ -3,6 +3,13 @@ require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = '4Bp3ON6lBN7sUZbvQ/12pOlUnWMjSg9on7Avj6vmtll013Km4kPtGZvJhblSiV8ok0i94zqDSM9n0ofJt05ro0/DgAAFVuE96jncEBTbtieuV89VBPoqPzDhfN+Mvyr8n0r8iExQDgmXV3Yo9D9IZQdB04t89/1O/w1cDnyilFU=';
 
+$channelSecret = 'a442994d2bf1499fd9b2b771d362e7dd';
+
+$pushID = 'U31164826a61dc970957e3edc089f87c0';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -38,7 +45,10 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $result . "\r\n";
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($result);
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+// echo $result . "\r\n";
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 }
 }
 }
