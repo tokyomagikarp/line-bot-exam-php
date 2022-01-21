322a1472739934c852ed5abfbd658c0db8a2ae65
tokyomagikarp <97447023+tokyomagikarp@users.noreply.github.com>
2022-01-21 13:11:51 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -13,7 +13,7 @@ $pushID = 'U31164826a61dc970957e3edc089f87c0';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
