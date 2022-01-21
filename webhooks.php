0befc6c761673566d7a47a4b601d91b38d4ba04d
tokyomagikarp <97447023+tokyomagikarp@users.noreply.github.com>
2022-01-21 14:16:36 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -45,7 +45,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($events);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 // echo $result . "\r\n";
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
