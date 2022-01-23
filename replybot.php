6eab9da40a0beba29987deac2e95007c6d058ba1
tokyomagikarp <97447023+tokyomagikarp@users.noreply.github.com>
2022-01-23 15:59:50 +0700

    Update replybot.php

--- replybot.php
+++ replybot.php
@@ -32,7 +32,7 @@ $replyToken = $event['replyToken'];
 // Build message to reply back
 $messages = [
 'type' => 'text',
-'text' => $usertext . "<br> dsadasdas"
+'text' => $usertext . "<html><br/> dsadasdas</html>"
 ];
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';
